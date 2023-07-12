37202c258508e76c700314d9467e8716f98fd359
Roddvoyage <joao.f.rodrigues.93@gmail.com>
2023-07-12 01:48:20 +0100

    new fonts and first steps towards behavioral page

---
+++ javascript.js
@@ -0,0 +1,28 @@
+const rockButton = document.getElementById('weaponImg-rock');
+const paperButton = document.getElementById('weaponImg-paper');
+const scissorsButton = document.getElementById('weaponImg-scissors');
+
+
+function Rock2() {
+    rockButton.src = 'rock2.png';
+}
+
+function Rock1() {
+    rockButton.src = 'rock1.png';
+}
+
+function Paper2() {
+    paperButton.src = 'paper2.png';
+}
+
+function Paper1() {
+    paperButton.src = 'paper1.png';
+}
+
+function Scissors2() {
+    scissorsButton.src = 'scissors2.png';
+}
+
+function Scissors1() {
+    scissorsButton.src = 'scissors1.png';
+}
