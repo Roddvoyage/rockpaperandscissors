288e52c72dce8ad2bdeac03da788e440dcb343f8
Roddvoyage <joao.f.rodrigues.93@gmail.com>
2023-07-12 22:14:44 +0100

    working game

--- javascript.js
+++ javascript.js
@@ -4,12 +4,21 @@ const scissorsButton = document.getElementById('weaponImg-scissors');
 const scissorsTitleButton = document.getElementById('scissorsTitle');
 const rockTitleButton = document.getElementById('rockTitle');
 const paperTitleButton = document.getElementById('paperTitle');
+const Player = document.getElementById('Player');
+const UPC = document.getElementById('UPCS');
 
+const whowon = document.getElementById('whowon');
+const bothplays = document.getElementById('bothplays')
 
+let playerSelection = '';
+let UPCSelection
+
+let MeScore = 0
+let ComputerScore = 0
 
 function Rock2() {
     rockButton.src = 'rock2.png';
-    rockTitleButton.style.color = 'pink';
+    rockTitleButton.style.color = '#b0589d';
 }
 
 function Rock1() {
@@ -19,13 +28,14 @@ function Rock1() {
 
 function Rock3() {
     rockButton.src = 'rock3.png';
-    rockTitleButton.style.color = 'purple';
+    rockTitleButton.style.color = '#312a6f';
+    playRock();
 }
 
 
 function Paper2() {
     paperButton.src = 'paper2.png';
-    paperTitleButton.style.color = 'green';
+    paperTitleButton.style.color = '#5abcaa';
 }
 
 function Paper1() {
@@ -35,7 +45,8 @@ function Paper1() {
 
 function Paper3() {
     paperButton.src = 'paper3.png';
-    paperTitleButton.style.color = 'purple';
+    paperTitleButton.style.color = '#312a6f';
+    playPaper();
 }
 
 
@@ -43,7 +54,7 @@ function Paper3() {
 
 function Scissors2() {
     scissorsButton.src = 'scissors2.png';
-    scissorsTitleButton.style.color = 'red';
+    scissorsTitleButton.style.color = '#3572b8';
 }
 
 function Scissors1() {
@@ -53,7 +64,112 @@ function Scissors1() {
 
 function Scissors3() {
     scissorsButton.src = 'scissors3.png';
-    scissorsTitleButton.style.color = 'purple';
+    scissorsTitleButton.style.color = '#312a6f';
+    playScissors();
 }
 
 
+
+
+
+
+function playRock(){
+    playerSelection = 'rock'
+    UPCSelection = Math.floor(Math.random() * 3);
+
+    if(UPCSelection === 0) {
+       console.log('draw');
+       bothplays.innerHTML = 'you chose rock and upc chose rock';
+       whowon.innerHTML = 'its a draw.'
+    } else if (UPCSelection === 2) {
+        MeScore++;
+        Player.textContent = MeScore;
+        bothplays.innerHTML = 'you chose rock and upc chose paper';
+        whowon.innerHTML = 'you won!'
+    } else if (UPCSelection === 1) {
+        ComputerScore++;
+        UPC.textContent = ComputerScore;
+        bothplays.innerHTML = 'you chose rock and upc chose scissors';
+        whowon.innerHTML = 'upc win...'
+    } 
+}
+
+function playPaper(){
+    playerSelection = 'paper'
+    UPCSelection = Math.floor(Math.random() * 3);
+
+    if(UPCSelection === 1) {
+       console.log('draw');
+       bothplays.innerHTML = 'you chose paper and upc chose paper';
+       whowon.innerHTML = 'its a draw.'
+    } else if (UPCSelection === 0) {
+        MeScore++;
+        Player.textContent = MeScore;
+        bothplays.innerHTML = 'you chose paper and upc chose rock';
+        whowon.innerHTML = 'you won!'
+    } else if (UPCSelection === 2) {
+        ComputerScore++;
+        UPC.textContent = ComputerScore;
+        bothplays.innerHTML = 'you chose paper and upc chose scissors';
+        whowon.innerHTML = 'upc win...'
+    } 
+}
+
+function playScissors(){
+    playerSelection = 'rock'
+    UPCSelection = Math.floor(Math.random() * 3);
+
+    if(UPCSelection === 2) {
+       console.log('draw');
+       bothplays.innerHTML = 'you chose scissors and upc chose scissors';
+       whowon.innerHTML = 'its a draw.'
+    } else if (UPCSelection === 1) {
+        MeScore++;
+        Player.textContent = MeScore;
+        bothplays.innerHTML = 'you chose scissors and upc chose paper';
+        whowon.innerHTML = 'you won!'
+    } else if (UPCSelection === 0) {
+        ComputerScore++;
+        UPC.textContent = ComputerScore;
+        bothplays.innerHTML = 'you chose scissors and upc chose rock';
+        whowon.innerHTML = 'upc win...'
+    } 
+}
+
+
+
+
+/*
+
+if(playerSelection === 'rock' && UPCSelection === 'rock') {
+    PlayerScore += '1';
+    playerSelection = '';
+    UPCSelection = '';
+    
+} else if (playerSelection === 'rock' && UPCSelection === 'scissors') {
+    PlayerScore += 1;
+    playerSelection = '';
+    UPCSelection = '';
+} else if (playerSelection === 'rock' && UPCSelection === 'paper') {
+    UPCScore += 1;
+    playerSelection = '';
+    UPCSelection = '';
+} 
+
+if(playerSelection === 'paper' && UPCSelection === 'paper') {
+    PlayerScore += 1;
+} else if (playerSelection === 'paper' && UPCSelection === 'rock') {
+    PlayerScore += 1;
+} else if (playerSelection === 'paper' && UPCSelection === 'scissors') {
+    UPCScore += 1;
+} 
+
+if(playerSelection === 'scissors' && UPCSelection === 'scissors') {
+    PlayerScore += 1;
+} else if (playerSelection === 'scissors' && UPCSelection === 'paper') {
+    PlayerScore += 1;
+} else if (playerSelection === 'scissors' && UPCSelection === 'rock') {
+    UPCScore += 1;
+} 
+
+*/
