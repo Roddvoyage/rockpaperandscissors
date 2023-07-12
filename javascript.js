f24d827e740bb65baff045e1702f2fcc6b51a550
Roddvoyage <joao.f.rodrigues.93@gmail.com>
2023-07-12 13:16:16 +0100

    add font color and clickevents

--- javascript.js
+++ javascript.js
@@ -1,28 +1,59 @@
 const rockButton = document.getElementById('weaponImg-rock');
 const paperButton = document.getElementById('weaponImg-paper');
 const scissorsButton = document.getElementById('weaponImg-scissors');
+const scissorsTitleButton = document.getElementById('scissorsTitle');
+const rockTitleButton = document.getElementById('rockTitle');
+const paperTitleButton = document.getElementById('paperTitle');
+
 
 
 function Rock2() {
     rockButton.src = 'rock2.png';
+    rockTitleButton.style.color = 'pink';
 }
 
 function Rock1() {
     rockButton.src = 'rock1.png';
+    rockTitleButton.style.color = 'white';
 }
+
+function Rock3() {
+    rockButton.src = 'rock3.png';
+    rockTitleButton.style.color = 'purple';
+}
+
 
 function Paper2() {
     paperButton.src = 'paper2.png';
+    paperTitleButton.style.color = 'green';
 }
 
 function Paper1() {
     paperButton.src = 'paper1.png';
+    paperTitleButton.style.color = 'white';
 }
+
+function Paper3() {
+    paperButton.src = 'paper3.png';
+    paperTitleButton.style.color = 'purple';
+}
+
+
+
 
 function Scissors2() {
     scissorsButton.src = 'scissors2.png';
+    scissorsTitleButton.style.color = 'red';
 }
 
 function Scissors1() {
     scissorsButton.src = 'scissors1.png';
+    scissorsTitleButton.style.color = 'white';
 }
+
+function Scissors3() {
+    scissorsButton.src = 'scissors3.png';
+    scissorsTitleButton.style.color = 'purple';
+}
+
+
